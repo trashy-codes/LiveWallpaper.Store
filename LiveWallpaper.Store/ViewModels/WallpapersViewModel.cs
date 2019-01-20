7e09f18199abb47c7bd8b98f2ef6cbe74b5b5b29
ying <120758737@qq.com>
2019-01-20 14:56:07 +0800

    save

--- LiveWallpaper.Store/ViewModels/WallpapersViewModel.cs
+++ LiveWallpaper.Store/ViewModels/WallpapersViewModel.cs
@@ -31,7 +31,8 @@ namespace LiveWallpaper.Store.ViewModels
         {
             base.OnViewLoaded();
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            LoadTagsAndSorts();
+            if (Wallpapers == null || Wallpapers.Count == 0)
+                LoadTagsAndSorts();
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
         }
